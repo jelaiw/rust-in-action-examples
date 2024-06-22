c943ed00640e1de49a73f733101a9c4efe792b46
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-06-22 15:10:01 +0000

    Note subtle behavior change with BufReader::lines(), which removes trailling newline.

--- ch2/bufreader-lines/src/main.rs
+++ ch2/bufreader-lines/src/main.rs
@@ -6,7 +6,11 @@ fn main() {
     let f = File::open("Cargo.toml").unwrap();
     let reader = BufReader::new(f);
 
+    // A subtle behavior change occurs here. BufReader::lines()
+    // removes the trailing newline character from each line.
     for line_ in reader.lines() {
+        // Unwraps the Result, but at the risk of crashing the
+        // program if an error occurs.
         let line = line_.unwrap();
         println!("{} ({} bytes long)", line, line.len());
     }
