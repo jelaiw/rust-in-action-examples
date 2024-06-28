ac6d25dbe2e2c8b48112457e24f0036c909085a0
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-06-28 22:44:59 +0000

    Code comments for type alias, relaxing compiler warnings, and ! return type.

--- ch3/not-quite-file/src/main.rs
+++ ch3/not-quite-file/src/main.rs
@@ -1,5 +1,8 @@
+// Relaxes compiler warnings while working through ideas.
 #![allow(unused_variables)]
 
+// Creates a type alias. The compiler won’t distinguish
+// between String & File, but your source code will.
 type File = String;
 
 fn open(f: &mut File) -> bool {
@@ -10,12 +13,17 @@ fn close(f: &mut File) -> bool {
     true
 }
 
+// Relaxes a compiler warning about an unused function.
 #[allow(dead_code)]
+// The ! return type indicates to the Rust compiler that
+// this function never returns.
 fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
+    // A macro that crashes the program if it’s encountered.
     unimplemented!()
 }
 
 fn main() {
+    // With the type declaration, File inherits all of String’s methods.
     let mut f1 = File::from("f1.txt");
     open(&mut f1);
     //read(f1, vec![]);
