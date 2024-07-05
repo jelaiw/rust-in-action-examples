56acdfda39b878692fcdad7cd46d74c0490cb17e
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-07-05 00:07:43 +0000

    Update with File struct. Simulate reading file contents.

--- ch3/not-quite-file/src/main.rs
+++ ch3/not-quite-file/src/main.rs
@@ -1,9 +1,11 @@
 // Relaxes compiler warnings while working through ideas.
 #![allow(unused_variables)]
 
-// Creates a type alias. The compiler won’t distinguish
-// between String & File, but your source code will.
-type File = String;
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
 
 fn open(f: &mut File) -> bool {
     true
@@ -13,19 +15,31 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-// Relaxes a compiler warning about an unused function.
-#[allow(dead_code)]
-// The ! return type indicates to the Rust compiler that
-// this function never returns.
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! {
-    // A macro that crashes the program if it’s encountered.
-    unimplemented!()
+fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length);
+    save_to.append(&mut tmp);
+    read_length
 }
 
 fn main() {
-    // With the type declaration, File inherits all of String’s methods.
-    let mut f1 = File::from("f1.txt");
-    open(&mut f1);
-    //read(f1, vec![]);
-    close(&mut f1);
+    let mut f2 = File {
+        name: String::from("2.txt"),
+        data: vec![114, 117, 115, 116, 33],
+
+    };
+
+    let mut buffer: Vec<u8> = vec![];
+
+    open(&mut f2);
+    let f2_length = read(&f2, &mut buffer);
+    close(&mut f2);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{} is {} bytes long", &f2.name, f2_length);
+    println!("{}", text);
 }
