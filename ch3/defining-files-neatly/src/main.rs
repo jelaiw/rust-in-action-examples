77b88dd07dd1846cd0418ddf3b351448a8b7481a
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-07-06 01:23:38 +0000

    Code comment on private by default struct fields and nothing special about new() function.

--- ch3/defining-files-neatly/src/main.rs
+++ ch3/defining-files-neatly/src/main.rs
@@ -5,7 +5,11 @@ struct File {
 }
 
 impl File {
+    // As File::new() is a completely normal function, we need to 
+    // tell Rust that it will return a File from this function.
     fn new(name: &str) -> File {
+        // File::new() does little more than encapsulate 
+        // the object creation syntax, which is normal.
         File {
             name: String::from(name),
             data: Vec::new(),
@@ -16,6 +20,8 @@ impl File {
 fn main() {
     let f3 = File::new("f3.txt");
 
+    // Fields are private by default but can be accessed 
+    // within the module that defines the struct.
     let f3_name = &f3.name;
     let f3_length = &f3.data.len();
 
