92759938eeb025d3155e7564df62625a8a4c7fba
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-07-08 14:47:37 +0000

    Code comment on self.

--- ch3/defining-files-neatly/src/main.rs
+++ ch3/defining-files-neatly/src/main.rs
@@ -18,12 +18,15 @@ impl File {
         }
     }
 
+    // This method sneaked in to deal with cases where we want
+    // to simulate that a file has pre-existing data.
     fn new_with_data(name: &str, data: &Vec<u8>) -> File {
         let mut f = File::new(name);
         f.data = data.clone();
         f
     }
 
+    // Note f argument replaced with self.
     fn read(self: &File, save_to: &mut Vec<u8>,) -> usize {
         let mut tmp = self.data.clone();
         let read_length = tmp.len();
@@ -42,6 +45,8 @@ fn close(f: &mut File) -> bool {
 }
 
 fn main() {
+    // An explicit type needs to be provided as vec! and can’t infer
+    // the necessary type through the function boundary.
     let f3_data: Vec<u8> = vec![114, 117, 115, 116, 33];
     let mut f3 = File::new_with_data("f3.txt", &f3_data);
 
@@ -50,6 +55,7 @@ fn main() {
     // Fields are private by default but can be accessed 
     // within the module that defines the struct.
     open(&mut f3);
+    // Guess this means self has special meaning in Rust??
     let f3_length = f3.read(&mut buffer);
     close(&mut f3);
 
