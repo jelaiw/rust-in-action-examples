fbdf44bf0d16be830ef63bb6771683e58d857cf9
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-07-15 22:59:08 +0000

    Adapt code comments from Listing 3.6.

--- ch3/globalerror/src/main.rs
+++ ch3/globalerror/src/main.rs
@@ -1,5 +1,7 @@
 use rand::random;
 
+// A global variable, static mut (or mutable static), with a
+// static lifetime that’s valid for the life of the program.
 static mut ERROR: isize = 0;
 
 // Creates a zero-sized type to stand in for a struct
@@ -10,6 +12,9 @@ struct File;
 fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
     // Returns true one out of eight times this function is called.
     if random() && random() && random() {
+        // Accessing and modifying static mut variables requires
+        // the use of an unsafe block. This is Rust’s way of
+        // disclaiming all responsibility.
         unsafe {
             ERROR = 1;
         }
@@ -27,6 +32,7 @@ fn main() {
     read(&f, &mut buffer);
     // Accessing static mut variables is an unsafe operation.
     unsafe {
+        // Error checking relies on the convention that 0 means no error.
         if ERROR != 0 {
             panic!("An error has occurred!")
         }
