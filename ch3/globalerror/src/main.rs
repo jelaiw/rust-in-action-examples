76f667c49fcc33019f77965ed88219337d36ad64
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-07-15 22:54:54 +0000

    Clarify zero-sized type, allow(unused_mut), and random intent with code comments.

--- ch3/globalerror/src/main.rs
+++ ch3/globalerror/src/main.rs
@@ -2,10 +2,13 @@ use rand::random;
 
 static mut ERROR: isize = 0;
 
+// Creates a zero-sized type to stand in for a struct
+// while we’re experimenting.
 struct File;
 
 #[allow(unused_variables)]
 fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
+    // Returns true one out of eight times this function is called.
     if random() && random() && random() {
         unsafe {
             ERROR = 1;
@@ -14,12 +17,15 @@ fn read(f: &File, save_to: &mut Vec<u8>) -> usize {
     0
 }
 
+// Keeping buffer mutable for consistency with other code
+// even though it isn’t touched here.
 #[allow(unused_mut)]
 fn main() {
     let mut f = File;
     let mut buffer = vec![];
 
     read(&f, &mut buffer);
+    // Accessing static mut variables is an unsafe operation.
     unsafe {
         if ERROR != 0 {
             panic!("An error has occurred!")
