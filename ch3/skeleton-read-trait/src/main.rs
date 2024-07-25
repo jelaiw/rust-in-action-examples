20160a294c343e4be3086674d2b1c8da39ae958b
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-07-25 00:17:48 +0000

    Comment on pseudo-type Self as a placeholder for eventual type that implements trait.

--- ch3/skeleton-read-trait/src/main.rs
+++ ch3/skeleton-read-trait/src/main.rs
@@ -3,12 +3,15 @@
 #[derive(Debug)]
 struct File;
 
+// A trait block includes the type signatures of functions that implementors must comply with. 
 trait Read {
+    // The pseudo-type Self is a placeholder for the type that eventually implements Read.
     fn read(self: &Self, save_to: &mut Vec<u8>,) -> Result<usize, String>;
 }
 
 impl Read for File {
     fn read(self: &File, save_to: &mut Vec<u8>,) -> Result<usize, String> {
+        // A simple stub value that complies with the type signature required.
         Ok(0)
     }
 }
