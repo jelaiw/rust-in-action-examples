e7e27c74f59e833865799757b8f4a26be8925846
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-08-02 23:36:21 +0000

    Code comments on ownership moves.

--- ch4/check-sats-2/src/main.rs
+++ ch4/check-sats-2/src/main.rs
@@ -14,16 +14,19 @@ fn check_status(sat_id: CubeSat) -> StatusMessage {
 }
 
 fn main() {
+    // Ownership originates here at the creation of the CubeSat object.
     let sat_a = CubeSat { id: 0 };
     let sat_b = CubeSat { id: 1 };
     let sat_c = CubeSat { id: 2 };
 
+    // Ownership of the object moves to check_status() but is not returned to main().
     let a_status = check_status(sat_a);
     let b_status = check_status(sat_b);
     let c_status = check_status(sat_c);
     println!("a: {:?}, b: {:?}, c: {:?}", a_status, b_status, c_status);
 
     // "waiting" ...
+    // sat_a is no longer the owner of the object, making access invalid.
     let a_status = check_status(sat_a);
     let b_status = check_status(sat_b);
     let c_status = check_status(sat_c);
