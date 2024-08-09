7f1c2c40cde5f0f414764d415579ee00407aad99
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-08-09 01:37:07 +0000

    Code comments from examples.

--- ch4/copy_semantics.rs
+++ ch4/copy_semantics.rs
@@ -1,3 +1,5 @@
+// use_value() takes ownership of the _val argument.
+// The use_value() function is generic as it’s used in the next example.
 fn use_value(_val: i32) {
 }
 
@@ -5,5 +7,6 @@ fn main() {
     let a = 123;
     use_value(a);
 
+    // It’s perfectly legal to access a after use_value() has returned.
     println!("{}", a);
 }
--- ch4/move_semantics.rs
+++ ch4/move_semantics.rs
@@ -2,6 +2,7 @@ struct Demo {
     a: i32,
 }
 
+// use_value() takes ownership of _val.
 fn use_value(_val: Demo) {
 }
 
@@ -9,5 +10,6 @@ fn main() {
     let demo = Demo { a: 123 };
     use_value(demo);
 
+    // It’s illegal to access demo.a, even after use_value() has returned.
     println!("{}", demo.a);
 }
