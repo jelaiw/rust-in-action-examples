4b822447337984b31dc122c376aa7694214f8a56
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-08-10 02:25:37 +0000

    Avoid ownership issues with references.

---
+++ ch4/sat-mailbox/src/main.rs
@@ -0,0 +1,50 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct CubeSat {
+    id: u64,
+    mailbox: Mailbox,
+}
+
+#[derive(Debug)]
+struct Mailbox {
+    messages: Vec<Message>,
+}
+
+type Message = String;
+
+struct GroundStation;
+
+impl GroundStation {
+    fn send(&self, to: &mut CubeSat, msg: Message) {
+        to.mailbox.messages.push(msg);
+    }
+}
+
+impl CubeSat {
+    // https://doc.rust-lang.org/stable/core/option/index.html
+    fn recv(&mut self) -> Option<Message> {
+        self.mailbox.messages.pop()
+    }
+}
+
+fn main() {
+    let base = GroundStation {};
+    let mut sat_a = CubeSat {
+        id: 0,
+        mailbox: Mailbox {
+            messages: vec![],
+        },
+    };
+
+    println!("t0: {:?}", sat_a);
+
+    base.send(&mut sat_a, Message::from("hello there!"));
+
+    println!("t1: {:?}", sat_a);
+
+    let msg = sat_a.recv();
+    println!("t2: {:?}", sat_a);
+
+    println!("msg: {:?}", msg);
+}
