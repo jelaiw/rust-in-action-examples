af29a8bc4b5ea31e2fa4043186d2a2828ef2cb34
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-08-22 00:26:50 +0000

    Use Copy and Clone traits.

---
+++ ch4/check-sats-clone-and-copy-traits/src/main.rs
@@ -0,0 +1,25 @@
+#![allow(unused_variables, dead_code)]
+
+#[derive(Debug, Clone, Copy)]
+struct CubeSat {
+    id: u64,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum StatusMessage {
+    Ok,
+}
+
+fn check_status(sat_id: CubeSat) -> StatusMessage {
+    StatusMessage::Ok
+}
+
+fn main() {
+    let sat_a = CubeSat { id: 0 };
+
+    let a_status = check_status(sat_a);
+    println!("a: {:?}", a_status);
+
+    let a_status = check_status(sat_a.clone());
+    println!("a: {:?}", a_status.clone());
+}
