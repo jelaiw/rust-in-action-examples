4810a5ba6c5087da1e398c41ccfd218e91d7bd74
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-08-23 23:12:25 +0000

    Code comments on macros, Unicode support, and borrow for read-only access.

--- ch1/hello2/src/main.rs
+++ ch1/hello2/src/main.rs
@@ -1,9 +1,15 @@
 fn greet_world() {
+    // The exclamation mark indicates the use of a macro.
     println!("Hello, world!");
+    // Assignment in Rust, more properly called variable binding, uses the let keyword.
     let southern_germany = "Grüß Gott!";
+    // Unicode support is provided out of the box.
     let japan = "ハロー・ワールド";
+    // Array literals use square brackets.
     let regions = [southern_germany, japan];
+    // Many types can have an iter() method to return an iterator.
     for region in regions.iter() {
+        // The ampersand “borrows” region for read-only access.
         println!("{}", &region);
     }
 }
