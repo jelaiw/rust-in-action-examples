02da3f5c7919c5d19917b8eb5812ee2a98e31e41
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-08-23 00:43:54 +0000

    Code comment on enclosing the GroundStation instance in a call to Rc::new().

--- ch4/rc-groundstation/src/main.rs
+++ ch4/rc-groundstation/src/main.rs
@@ -4,6 +4,7 @@ use std::rc::Rc;
 struct GroundStation;
 
 fn main() {
+    // Wrapping involves enclosing the GroundStation instance in a call to Rc::new().
     let base = Rc::new(GroundStation {});
 
     println!("{:?}", base);
