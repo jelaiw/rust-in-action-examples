2dbf583db11dd1d11028edb03b8d1db15dc0d2f6
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-08-30 01:10:02 +0000

    Attempt to create a dangling pointer.
    Update with code comments from book.

--- ch1/cereals/main.rs
+++ ch1/cereals/main.rs
@@ -1,12 +1,17 @@
+// Allows the println! macro to print the Cereal enum.
 #[derive(Debug)]
+// An enum (enumeration) is a type with a fixed number of legal variants.
 enum Cereal {
     Barley, Millet, Rice,
     Rye, Spelt, Wheat,
 }
 
 fn main() {
+    // Initializes an empty vector of Cereal.
     let mut grains: Vec<Cereal> = vec![];
     grains.push(Cereal::Rye);
+    // Deletes grains and its contents.
     drop(grains);
+    // Attempts to access the deleted value.
     println!("{:?}", grains);
 }
