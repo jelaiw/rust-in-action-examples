aa6b97978e208bfd233e8009c08f2ba262c36f8e
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-01 02:36:50 +0000

    Code comment on closure syntax for map, eprintln for printing to stderr, cfg! and turbofish syntax for type annotation.

--- ch1/penguins/src/main.rs
+++ ch1/penguins/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // Escapes the trailing newline character.
     let penguin_data = "\
     common name,length (cm)
     Little penguin,33
@@ -10,20 +11,24 @@ fn main() {
     let records = penguin_data.lines();
 
     for (i, record) in records.enumerate() {
+        // Skips header row and lines with only whitespace.
         if i == 0 || record.trim().len() == 0 {
             continue;
         }
 
-        let fields: Vec<_> = record
-            .split(',')
-            .map(|field| field.trim())
-            .collect();
+        let fields: Vec<_> = record // Starts with a line of text.
+            .split(',') // Splits record into fields.
+            .map(|field| field.trim()) // Trims whitespace of each field.
+            .collect(); // Builds a collection of fields.
         
+        // cfg! checks configuration at compile time.
         if cfg!(debug_assertions) {
+            // eprintln! prints to standard error (stderr).
             eprintln!("debug: {:?} -> {:?}", record, fields);
         }
 
         let name = fields[0];
+        // Attempts to parse field as a floating-point number.
         if let Ok(length) = fields[1].parse::<f32>() {
             println!("{}, {}cm", name, length);
         }
