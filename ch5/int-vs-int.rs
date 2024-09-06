0bac2891b04aabe809e647863842f0d2f8c2821e
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-06 00:02:51 +0000

    Link to format spec.

--- ch5/int-vs-int.rs
+++ ch5/int-vs-int.rs
@@ -2,6 +2,7 @@ fn main() {
     let a: u16 = 50115;
     let b: i16 = -15421;
 
+    // See https://doc.rust-lang.org/std/fmt/#formatting-traits for refresher.
     println!("a: {:016b} {}", a, a);
     println!("b: {:016b} {}", b, b);
 }
