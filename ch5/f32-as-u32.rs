f751aec1bfe9755fb8baef63bd561f32ceab0776
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-07 20:15:28 +0000

    Code comments on lack of semicolon to feed expression result to outer scope.

--- ch5/f32-as-u32.rs
+++ ch5/f32-as-u32.rs
@@ -1,15 +1,19 @@
 fn main() {
     let a: f32 = 42.42;
     let frankentype: u32 = unsafe {
+        // No semicolon here. We want the result of this expression to feed into the outer scope.
         std::mem::transmute(a)
     };
 
+    // Views the bits of a 42.42_f32 value as a decimal integer.
     println!("{}", frankentype);
+    // {:032b} means to format as a binary via the std::fmt::Binary trait with 32 zeroes padded on the left.
     println!("{:032b}", frankentype);
 
     let b: f32 = unsafe {
         std::mem::transmute(frankentype)
     };
     println!("{}", b);
+    // Confirms that the operation is symmetrical.
     assert_eq!(a, b);
 }
