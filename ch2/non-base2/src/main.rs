f68943e2b5c3842dd0181bf81ab5ca0dcf172d75
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-11 23:23:17 +0000

    No surprises here, standard binary, octal, and hex prefix for integer literals.

--- ch2/non-base2/src/main.rs
+++ ch2/non-base2/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let three = 0b11;
-    let thirty = 0o36;
-    let three_hundred = 0x12C;
+    let three = 0b11; // The 0b prefix indicates binary (base 2) numerals.
+    let thirty = 0o36; // The 0o prefix indicates octal (base 8) numerals.
+    let three_hundred = 0x12C; // The 0x prefix indicates hexadecimal (base 16) numerals.
 
     println!("base 10: {} {} {}", three, thirty, three_hundred);
     println!("base 2: {:b} {:b} {:b}", three, thirty, three_hundred);
