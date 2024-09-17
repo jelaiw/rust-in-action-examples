ed511eae4bebbf847a7179429603f0dfa5647757
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-17 00:00:45 +0000

    Code comment on type annotation.

--- ch5/impossible-addition.rs
+++ ch5/impossible-addition.rs
@@ -1,5 +1,6 @@
 fn main() {
     let (a, b) = (200, 200);
+    // Without the type declaration, Rust won’t assume that you’re trying to create an impossible situation.
     let c: u8 = a + b;
 
     println!("{} + {} = {}", a, b, c);
