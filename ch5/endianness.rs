7531432e62dcf125d13bb8ab2abd39427e899e43
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-20 00:30:47 +0000

    std::mem::transmute() instructs the compiler to interpret its argument as the type on the left (i32).

--- ch5/endianness.rs
+++ ch5/endianness.rs
@@ -4,6 +4,7 @@ fn main() {
     let big_endian: [u8; 4] = [0xAA, 0xBB, 0xCC, 0xDD];
     let little_endian: [u8; 4] = [0xDD, 0xCC, 0xBB, 0xAA];
 
+    // std::mem::transmute() instructs the compiler to interpret its argument as the type on the left (i32).
     let a: i32 = unsafe { transmute(big_endian) };
     let b: i32 = unsafe { transmute(little_endian) };
 
