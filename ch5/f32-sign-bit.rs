be3ecfa32493fe4f85a90107f7e8004b8094aab8
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-27 00:50:45 +0000

    Assert expected bit pattern for 42.42_f32.

--- ch5/f32-sign-bit.rs
+++ ch5/f32-sign-bit.rs
@@ -3,6 +3,7 @@ fn main() {
     // Interpret bits of f32 as u32 to allow bit manipulation.
     // https://doc.rust-lang.org/std/primitive.f32.html#method.to_bits
     let bits: u32 = n.to_bits();
+    assert_eq!(0x4229ae14, bits);
     let sign_bit = bits >> 31;
 
     println!("floating point number: {}", n);
