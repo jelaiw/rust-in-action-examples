dd61b461bb8c9342f40661136685b121f786cf74
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-27 00:46:36 +0000

    Interpret bits of f32 as u32 to allow bit manipulation.

--- ch5/f32-sign-bit.rs
+++ ch5/f32-sign-bit.rs
@@ -1,5 +1,7 @@
 fn main() {
     let n: f32 = 42.42;
+    // Interpret bits of f32 as u32 to allow bit manipulation.
+    // https://doc.rust-lang.org/std/primitive.f32.html#method.to_bits
     let bits: u32 = n.to_bits();
     let sign_bit = bits >> 31;
 
