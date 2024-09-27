e13e6b471ff459514c14862995f9c2f7b5b79dd7
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-09-27 23:01:30 +0000

    Code comments on right shift, the AND mask, and decoding the exponent according to the IEEE 754 standard.

--- ch5/f32-exponent.rs
+++ ch5/f32-exponent.rs
@@ -5,14 +5,17 @@ fn main() {
     println!("floating point number: {}", n);
     println!("raw bits: {:032b}", bits);
     
+    // Right shift to overwrite mantissa.
     let exponent_ = bits >> 23;
     println!("sans mantissa: {:032b}", exponent_);
     // Flip the sign bit to make the mask step more interesting.
     let exponent_ = exponent_ | 0x100;
     println!("flip sign bit: {:032b}", exponent_);
+    // Filter out sign bit with AND mask.
     let exponent_ = exponent_ & 0xff;
     println!("sans sign bit: {:032b}", exponent_);
 
+    // Interpret the remaining bits as a signed integer and subtract the bias as defined by the standard.
     let bias = 127i32;
     let exponent = (exponent_ as i32) - bias;
     println!("exponent: {}", exponent);
