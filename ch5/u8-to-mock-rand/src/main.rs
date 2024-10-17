b28532699790edb7fdfe3853cd199145ab734a08
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-10-17 00:02:17 +0000

    Code comments explaining how output range is normalized (and more) in mock_rand.

--- ch5/u8-to-mock-rand/src/main.rs
+++ ch5/u8-to-mock-rand/src/main.rs
@@ -1,8 +1,13 @@
 fn mock_rand(n: u8) -> f32 {
     let base: u32 = 0b0_01111110_00000000000000000000000;
+    // Aligns the input byte n to 32 bits, then increases its value
+    // by shifting its bits 15 places to the left.
     let large_n = (n as u32) << 15;
+    // Takes a bitwise OR, merging the base with the input byte.
     let f32_bits = base | large_n;
+    // Interprets f32_bits (which is type u32) as an f32.
     let m = f32::from_bits(f32_bits);
+    // Normalizes the output range.
     2.0 * ( m - 0.5 )
 }
 
