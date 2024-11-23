5ac5d82c9e5588070435cc94bfea45c43ab330a9
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-11-23 13:05:07 -0600

    Demonstrate creating a pointer of arbitrary type from any integer is legal.
    Link to https://doc.rust-lang.org/std/primitive.pointer.html#method.offset.

--- ch6/listing-6_6.rs
+++ ch6/listing-6_6.rs
@@ -2,6 +2,7 @@ fn main() {
     let ptr = 42 as *const Vec<String>;
 
     unsafe {
+        // https://doc.rust-lang.org/std/primitive.pointer.html#method.offset.
         let addr = ptr.offset(4);
         println!("{:p} -> {:p}", ptr, addr);
     }
