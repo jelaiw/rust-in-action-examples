ffaa6e4e931a1c6ec9affb03903c00a5e2e6ef29
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-12-03 23:56:53 +0000

    Cast a reference to the variable a (&a) to a constant raw pointer i64 (*const i64).

--- ch6/listing-6_4.rs
+++ ch6/listing-6_4.rs
@@ -1,5 +1,6 @@
 fn main() {
     let a: i64 = 42;
+    // Casts a reference to the variable a (&a) to a constant raw pointer i64 (*const i64).
     let a_ptr = &a as *const i64;
 
     println!("a: {} ({:p})", a, a_ptr);
