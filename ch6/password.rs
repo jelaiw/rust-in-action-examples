ab613edb0c024697ef152d1fa54da50666584ac3
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-12-05 00:16:33 +0000

    Try implicit conversion to String.

--- ch6/password.rs
+++ ch6/password.rs
@@ -1,7 +1,11 @@
 // fn x<T: AsRef<str>>(a: T) reads as "function x takes argument a of
 // type T, where T implements AsRef<str>".
-fn is_strong<T: AsRef<str>>(password: T) -> bool {
-    password.as_ref().len() > 5
+//fn is_strong<T: AsRef<str>>(password: T) -> bool {
+//    password.as_ref().len() > 5
+//}
+
+fn is_strong<T: Into<String>>(password: T) -> bool {
+    password.into().len() > 5
 }
 
 fn main() {
