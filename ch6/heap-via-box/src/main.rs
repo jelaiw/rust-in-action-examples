7dd835b45998e35d24b14866e3cb67f57ed6d7ea
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-12-06 23:56:38 +0000

    Code comment on manual drop and dereference operator.

--- ch6/heap-via-box/src/main.rs
+++ ch6/heap-via-box/src/main.rs
@@ -1,11 +1,16 @@
+// Brings manual drop() into local scope.
 use std::mem::drop;
 
 fn main() {
+    // Allocates values on the heap.
     let a = Box::new(1);
     let b = Box::new(1);
     let c = Box::new(1);
 
+    // The unary *, the dereference operator, returns the value within the box.
     let result1 = *a + *b + *c;
+
+    // Invokes drop(), freeing memory for other uses.
     drop(a);
 
     let d = Box::new(1);
