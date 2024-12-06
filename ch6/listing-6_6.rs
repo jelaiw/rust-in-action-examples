c78087e1c0981436e7f11184847f75f1eab06386
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-12-06 23:51:51 +0000

    Dereference pointer to 60 which is an integer boxed on the heap.

--- ch6/listing-6_6.rs
+++ ch6/listing-6_6.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let a: i32 = 40;
-    let b: Box<i32> = Box::new(60);
+    let a: i32 = 40; // 40 lives on the stack.
+    let b: Box<i32> = Box::new(60); // 60 lives on the heap.
 
-    println!("{} + {} = {}", a, b, a + *b);
+    println!("{} + {} = {}", a, b, a + *b); // To access 60, we need to dereference it.
 }
