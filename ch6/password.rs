00f62f9976bf2d0f3bcf5940e3924848a5ece8c4
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-12-06 23:50:45 +0000

    Show that is_strong takes both str and String arguments.

--- ch6/password.rs
+++ ch6/password.rs
@@ -1,14 +1,14 @@
 // fn x<T: AsRef<str>>(a: T) reads as "function x takes argument a of
 // type T, where T implements AsRef<str>".
-//fn is_strong<T: AsRef<str>>(password: T) -> bool {
-//    password.as_ref().len() > 5
-//}
-
-fn is_strong<T: Into<String>>(password: T) -> bool {
-    password.into().len() > 5
+fn is_strong<T: AsRef<str>>(password: T) -> bool {
+    password.as_ref().len() > 5
 }
+
+//fn is_strong<T: Into<String>>(password: T) -> bool {
+//    password.into().len() > 5
+//}
 
 fn main() {
     let pw = "justok";
-    let is_strong = is_strong(pw);
+    println!("{} {}", is_strong(pw), is_strong( String::from(pw) ));
 }
