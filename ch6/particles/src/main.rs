eefdaab0cb7b6f52b075c663dd2c6d5405c855d5
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-12-11 02:27:44 +0000

    Implement new and update for Particle.

--- ch6/particles/src/main.rs
+++ ch6/particles/src/main.rs
@@ -4,8 +4,9 @@ use std::alloc::{GlobalAlloc, Layout, System};
 use std::time::Instant;
 // rand provides random number generators and related functionality.
 use rand::prelude::ThreadRng;
+use rand::{thread_rng, Rng};
 // graphics::math::Vec2d provides mathematical operations and conversion functionality for 2D vectors.
-use graphics::math::Vec2d;
+use graphics::math::{Vec2d, add, mul_scalar};
 
 // #[global_allocator] marks the following value (ALLOCATOR) as satisfying the GlobalAlloc trait.
 #[global_allocator]
@@ -49,6 +50,34 @@ struct Particle {
     velocity: Vec2d<f64>,
     acceleration: Vec2d<f64>,
     color: [f32; 4],
+}
+
+impl Particle {
+    fn new(world: &World) -> Particle {
+        let mut rng = thread_rng();
+        let x = rng.gen_range(0.0..=world.width);
+        let y = world.height;
+        let x_velocity = 0.0;
+        let y_velocity = rng.gen_range(-2.0..0.0);
+        let x_acceleration = 0.0;
+        let y_acceleration = rng.gen_range(0.0..0.15);
+
+        Particle {
+            height: 4.0,
+            width: 4.0,
+            position: [x, y].into(),
+            velocity: [x_velocity, y_velocity].into(),
+            acceleration: [x_acceleration, y_acceleration].into(),
+            color: [1.0, 1.0, 1.0, 0.99],
+        }
+    }
+
+    fn update(&mut self) {
+        self.velocity = add(self.velocity, self.acceleration);
+        self.position = add(self.position, self.velocity);
+        self.acceleration = mul_scalar(self.acceleration, 0.7);
+        self.color[3] *= 0.995;
+    }
 }
 
 fn main() {
