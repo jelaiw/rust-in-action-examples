d529594e0a24edbe415da278c2fb45921e1118e6
jelaiw <14236583+jelaiw@users.noreply.github.com>
2024-12-16 23:12:28 +0000

    Convert i to a *const T, a raw pointer of type u8 to inspect raw memory addresses.

--- ch6/memscan-1/src/main.rs
+++ ch6/memscan-1/src/main.rs
@@ -1,8 +1,12 @@
 fn main() {
     let mut n_nonzero = 0;
 
-    for i in 1..10000 {
+    for i in 0..10000 {
+        // Converts i to a *const T, a raw pointer of type u8 to inspect raw memory addresses.
+        // We treat every address as a unit, ignoring the fact that most values span multiple bytes.
         let ptr = i as *const u8;
+        // Dereferences the pointer, it reads the value at address i.
+        // Another way of saying this is “read the value being pointed to.”
         let byte_at_addr = unsafe { *ptr };
 
         if byte_at_addr != 0 {
