f5871346adbc90613770fd5570cb802d90382d50
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-01-06 23:27:23 +0000

    Serialize a Rust struct to bincode.

--- ch7/serde-eg/src/main.rs
+++ ch7/serde-eg/src/main.rs
@@ -1,6 +1,7 @@
 use serde_derive::Serialize;
 use serde_json::to_string as to_json;
 use serde_cbor::to_vec as to_cbor;
+use bincode::serialize as to_bincode;
 
 #[derive(Serialize)]
 struct City {
@@ -20,10 +21,13 @@ fn main() {
 
     let as_json = to_json(&calabar).unwrap();
     let as_cbor = to_cbor(&calabar).unwrap();
+    let as_bincode = to_bincode(&calabar).unwrap();
 
     println!("json:\n{}\n", &as_json);
     println!("cbor:\n{:?}\n", &as_cbor);
+    println!("bincode:\n{:?}\n", &as_bincode);
 
     println!("json (as UTF-8):\n{}\n", String::from_utf8_lossy(as_json.as_bytes()));
     println!("cbor (as UTF-8):\n{}\n", String::from_utf8_lossy(&as_cbor));
+    println!("bincode (as UTF-8):\n{}\n", String::from_utf8_lossy(&as_bincode));
 }
