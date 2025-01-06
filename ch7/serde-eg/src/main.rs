1d60c65325451ae2ad01ecf87173fc3c62b72ca1
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-01-06 23:06:52 +0000

    Serialize a Rust struct to JSON.

---
+++ ch7/serde-eg/src/main.rs
@@ -0,0 +1,23 @@
+use serde_derive::Serialize;
+use serde_json::to_string as to_json;
+
+#[derive(Serialize)]
+struct City {
+    name: String,
+    population: usize,
+    latitude: f64,
+    longitude: f64,
+}
+
+fn main() {
+    let calabar = City {
+        name: String::from("Calabar"),
+        population: 470_000,
+        latitude: 4.95,
+        longitude: 8.33,
+    };
+
+    let as_json = to_json(&calabar).unwrap();
+
+    println!("json:\n{}\n", &as_json);
+}
