2c4b830d9304015aeebec9aa3eddd47470b67f49
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-01-07 23:58:38 +0000

    Comment on prelude imports and multiline raw string literals.

--- ch7/fview-str/src/main.rs
+++ ch7/fview-str/src/main.rs
@@ -1,6 +1,13 @@
+// prelude imports heavily used traits such as Read and Write in I/O operations.
+// It’s possible to include the traits manually, but they’re so common that the
+// standard library provides this convenience line to help keep your code compact.
 use std::io::prelude::*;
 
 const BYTES_PER_LINE: usize = 16;
+// Multiline string literals don’t need double quotes escaped when built with
+// raw string literals (the r prefix and the # delimiters).
+// The additional b prefix indicates that this should be treated as bytes (&[u8])
+// not as UTF-8 text (&str).
 const INPUT: &'static [u8] = br#"
 fn main() {
     println!("Hello, world!");
@@ -12,6 +19,7 @@ fn main() -> std::io::Result<()> {
 
     let mut position_in_input = 0;
     for line in buffer.chunks(BYTES_PER_LINE) {
+        // Writes the current position with up to 8 left-padded zeros.
         print!("[0x{:08x}] ", position_in_input);
         for byte in line {
             print!("{:02x} ", byte);
