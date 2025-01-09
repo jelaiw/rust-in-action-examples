ca79fe2959f665d9d0a53ba7ffb1887baf6da902
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-01-09 00:18:37 +0000

    Link to API docs for Iterator.nth, Result.expect, and Read.read_exact.

--- ch7/fview/src/main.rs
+++ ch7/fview/src/main.rs
@@ -6,14 +6,17 @@ use std::env;
 const BYTES_PER_LINE: usize = 8;
 
 fn main() {
+    // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.nth
     let arg1 = env::args().nth(1);
 
+    // https://doc.rust-lang.org/std/result/enum.Result.html#method.expect
     let fname = arg1.expect("usage: fview FILENAME");
 
     let mut f = File::open(&fname).expect("Unable to open file.");
     let mut pos = 0;
     let mut buffer = [0; BYTES_PER_LINE];
 
+    // https://doc.rust-lang.org/std/io/trait.Read.html#method.read_exact
     while let Ok(_) = f.read_exact(&mut buffer) {
         print!("[0x{:08x}] ", pos);
         for byte in &buffer {
