d5cea5a3fc6931075e233859dbea41d6f8ceb5a4
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-01-21 23:44:55 +0000

    Try conditional compilation.

---
+++ ch7/actionkv/src/akv_mem.rs
@@ -0,0 +1,29 @@
+#[cfg(target_os = "windows")]
+const USAGE: &str = "
+Usage:
+    akv_mem.exe FILE get KEY
+    akv_mem.exe FILE delete KEY
+    akv_mem.exe FILE insert KEY VALUE
+    akv_mem.exe FILE update KEY VALUE
+";
+
+#[cfg(not(target_os = "windows"))]
+const USAGE: &str = "
+Usage:
+    akv_mem FILE get KEY
+    akv_mem FILE delete KEY
+    akv_mem FILE insert KEY VALUE
+    akv_mem FILE update KEY VALUE
+";
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let fname = args.get(1).expect(&USAGE);
+    let action = args.get(2).expect(&USAGE).as_ref();
+
+    match action {
+        "get" => println!("{} {}", fname, action),
+
+        _ => eprintln!("{}", &USAGE),
+    }
+}
