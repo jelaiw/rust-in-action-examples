663fdbd5c87d6262ec72d8c4d59eea5a6d4ed7e5
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-01-24 00:44:46 +0000

    Implement ActionKV::open().

--- ch7/actionkv/src/lib.rs
+++ ch7/actionkv/src/lib.rs
@@ -1,3 +1,40 @@
+use serde_derive::{Serialize, Deserialize};
+use std::collections::HashMap;
+use std::fs::{File, OpenOptions};
+use std::path::Path;
+
+type ByteString = Vec<u8>;
+type ByteStr = [u8];
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KeyValuePair {
+    pub key: ByteString,
+    pub value: ByteString,
+}
+
+#[derive(Debug)]
+pub struct ActionKV {
+    f: File,
+    pub index: HashMap<ByteString, u64>,
+}
+
+impl ActionKV {
+    pub fn open(path: &Path) -> std::io::Result<Self> {
+        let f= OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .append(true)
+            .open(path)?;
+        let index = HashMap::new();
+
+        Ok(ActionKV { 
+            f,
+            index,
+        })
+    }
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
