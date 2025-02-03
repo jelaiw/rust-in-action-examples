b9f8de28ef3508bdc739d098f16639fad0a085ed
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-02-03 23:57:55 +0000

    All of Rust’s integer types come equipped with count_ones() and count_zeros() methods.

--- ch7/paritybit/src/main.rs
+++ ch7/paritybit/src/main.rs
@@ -1,12 +1,17 @@
+// Takes a byte slice as the bytes argument and returns a single byte as output. This function could have easily
+// returned a bool value, but returning u8 allows the result to bit shift into some future desired position.
 fn parity_bits(bytes: &[u8]) -> u8 {
     let mut n_ones: u32 = 0;
 
     for byte in bytes {
+        // All of Rust’s integer types come equipped with count_ones() and count_zeros() methods.
         let ones = byte.count_ones();
         n_ones += ones;
         println!("{} (0b{:08b}) has {} one bits", byte, byte, ones);
     }
 
+    // There are plenty of methods to optimize this function. One fairly simple approach is to hard code a
+    // const [u8; 256] array of 0s and 1s, corresponding to the intended result, then index that array with each byte.
     (n_ones % 2 == 0) as u8
 }
 
