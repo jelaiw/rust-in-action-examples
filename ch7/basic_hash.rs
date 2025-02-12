dc4ef86011ee8fe8188b8df77e81d974e00fb4c9
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-02-12 23:44:15 +0000

    The .chars() iterator converts the string into a series of char values, each 4 bytes long.

--- ch7/basic_hash.rs
+++ ch7/basic_hash.rs
@@ -1,7 +1,10 @@
 fn basic_hash(key: &str) -> u32 {
+    // The .chars() iterator converts the string into a series of char values, each 4 bytes long.
     let first = key.chars()
-        .next()
-        .unwrap_or('\0');
+        .next() // Returns an Option that’s either Some(char) or None for empty strings.
+        // unwrap_or() behaves as unwrap() but provides a value rather than panicking when it encounters None.
+        .unwrap_or('\0'); // If an empty string, provides NULL as the default.
+    // Interprets the memory of first as an u32, even though its type is char.
     u32::from(first)
 }
 
