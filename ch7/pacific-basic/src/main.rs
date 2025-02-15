af7e5a4b1deb3eb8ded0b0298b93baf005e7757f
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-02-15 01:02:19 +0000

    HashMap implements Index, which allows for values to be retrieved via the square bracket indexing style.

--- ch7/pacific-basic/src/main.rs
+++ ch7/pacific-basic/src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 
 fn main() {
+    // Type declarations of keys and values are not required here as these are inferred by the Rust compiler.
     let mut capitals = HashMap::new();
 
     capitals.insert("Cook Islands", "Avarua");
@@ -10,6 +11,7 @@ fn main() {
     capitals.insert("Tonga", "Nuku'alofa");
     capitals.insert("Tuvalu", "Funafuti");
 
+    // HashMap implements Index, which allows for values to be retrieved via the square bracket indexing style.
     let tongan_capital = capitals["Tonga"];
     println!("The capital of Tonga is {}.", tongan_capital);
 }
