197d26fe9c173d254de30347cd5c016d2dfc8f7c
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-02-17 23:07:28 +0000

    Demonstrate range queries and ordered iteration of BTreeMap.

---
+++ ch7/voc.rs
@@ -0,0 +1,22 @@
+use std::collections::BTreeMap;
+
+fn main() {
+    let mut voc = BTreeMap::new();
+
+    voc.insert(1_300_405, "Middelburg");
+    voc.insert(3_697_915, "Amsterdam");
+    voc.insert(  173_000, "Rotterdam");
+    voc.insert(  469_400, "Delft");
+    voc.insert(  266_868, "Hoorn");
+    voc.insert(  540_000, "Enkhuizen");
+
+    for (guilders, kamer) in &voc {
+        println!("{} invested {}", kamer, guilders);
+    }
+
+    print!("smaller chambers: ");
+    for (_guilders, kamer) in voc.range(0..500_000) {
+        print!("{} ", kamer)
+    }
+    println!("");
+}
