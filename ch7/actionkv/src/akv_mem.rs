e84ae85342b048f2b66d046dae232f67525ee7c0
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-02-19 00:49:04 +0000

    Wire up akm_mem to libactionkv.

--- ch7/actionkv/src/akv_mem.rs
+++ ch7/actionkv/src/akv_mem.rs
@@ -1,3 +1,5 @@
+use libactionkv::ActionKV;
+
 // The cfg attribute allows Windows users to see the correct file extension in their help documentation.
 #[cfg(target_os = "windows")]
 const USAGE: &str = "
@@ -21,9 +23,30 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let fname = args.get(1).expect(&USAGE);
     let action = args.get(2).expect(&USAGE).as_ref();
+    let key = args.get(3).expect(&USAGE).as_ref();
+    let maybe_value = args.get(4);
+
+    let path = std::path::Path::new(&fname);
+    let mut store = ActionKV::open(path).expect("unable to open file");
+    store.load().expect("unable to load data");
 
     match action {
-        "get" => println!("{} {}", fname, action),
+        "get" => match store.get(key).unwrap() {
+            None => eprintln!("{:?} not found", key),
+            Some(value) => println!("{:?}", value),
+        },
+
+        "delete" => store.delete(key).unwrap(),
+
+        "insert" => {
+            let value = maybe_value.expect(&USAGE).as_ref();
+            store.insert(key, value).unwrap()
+        },
+
+        "update" => {
+            let value = maybe_value.expect(&USAGE).as_ref();
+            store.update(key, value).unwrap()
+        },
 
         _ => eprintln!("{}", &USAGE),
     }
