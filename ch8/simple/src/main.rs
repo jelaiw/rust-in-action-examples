5a30bf33360ae1bd8dbc463179e6a47345523d23
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-02-25 00:41:26 +0000

    Box<dyn Error> represents a trait object.

--- ch8/simple/src/main.rs
+++ ch8/simple/src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use reqwest;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> { // Box<dyn Error> represents a trait object.
     let url = "http://www.rustinaction.com";
     let mut response = reqwest::get(url)?;
 
