5b0a760ff5e6ad9a40cac4982a60cb31fc350b0a
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-03-12 23:38:23 +0000

    A trait object, Box<dyn Error>, represents any type that implements std::error::Error.

--- ch8/misc/traiterror.rs
+++ ch8/misc/traiterror.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::net::Ipv6Addr;
 use std::error::Error;
 
+// A trait object, Box<dyn Error>, represents any type that implements std::error::Error.
 fn main() -> Result<(), Box<dyn Error>> {
     // File::open() returns Result<(), std::io::Error>.
     // Doesn't File::open() return Result<File, std::io::Error>?
