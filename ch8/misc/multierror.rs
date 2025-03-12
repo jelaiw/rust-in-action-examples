0fc2c20ee8534d69c0e7a184b642f3d9b05608b8
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-03-12 00:27:56 +0000

    "".parse::<Ipv6Addr>() returns Result<Ipv6Addr, std::net::AddrParseError>.

--- ch8/misc/multierror.rs
+++ ch8/misc/multierror.rs
@@ -2,8 +2,10 @@ use std::fs::File;
 use std::net::Ipv6Addr;
 
 fn main() -> Result<(), std::io::Error> {
+    // File::open() returns Result<(), std::io::Error>.
     let _f:File = File::open("foo.txt")?;
 
+    // "".parse::<Ipv6Addr>() returns Result<Ipv6Addr, std::net::AddrParseError>.
     let _localhost = "::1".parse::<Ipv6Addr>()?;
 
     Ok(())
