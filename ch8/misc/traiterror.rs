2ebe42442c601970bf1b64139e1fc21bd45a4bea
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-03-12 23:36:31 +0000

    Use trait object for return value.

---
+++ ch8/misc/traiterror.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::net::Ipv6Addr;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // File::open() returns Result<(), std::io::Error>.
+    // Doesn't File::open() return Result<File, std::io::Error>?
+    let _f:File = File::open("foo.txt")?;
+
+    // "".parse::<Ipv6Addr>() returns Result<Ipv6Addr, std::net::AddrParseError>.
+    let _localhost = "::1".parse::<Ipv6Addr>()?;
+
+    Ok(())
+}
