34bb5b8c23695782d19f881c3435baa159cc5d95
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-03-18 23:43:05 +0000

    Link to map_err API documentation.

--- ch8/misc/wraperror.rs
+++ ch8/misc/wraperror.rs
@@ -18,6 +18,7 @@ impl std::fmt::Display for UpstreamError {
 impl std::error::Error for UpstreamError {}
 
 fn main() -> Result<(), UpstreamError> {
+    // https://doc.rust-lang.org/std/result/enum.Result.html#method.map_err
     let _f:File = File::open("foo.txt").map_err(UpstreamError::IO)?;
 
     let _localhost = "::1".parse::<Ipv6Addr>().map_err(UpstreamError::Parsing)?;
