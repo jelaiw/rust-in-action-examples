9bb9112874df4cfcb50d928cb84f7d05199ca04a
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-05-22 23:26:02 +0000

    Loop and quit after correct guess.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,22 +7,27 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
-    let num_bytes = std::io::stdin().read_line(&mut guess).expect("Failed to read line");
-    dbg!(num_bytes);
+        let mut guess = String::new();
+        let num_bytes = std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+        dbg!(num_bytes);
 
-    let guess:u32 = guess
-        .trim()
-        .parse()
-        .expect("Please type a number!");
+        let guess:u32 = guess
+            .trim()
+            .parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!!");
+                break;
+            },
+        }
     }
 }
