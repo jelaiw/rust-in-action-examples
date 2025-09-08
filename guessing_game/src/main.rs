106265442fe8b9b2b895893d4326820a67f1d069
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-09-08 22:20:08 +0000

    Custom type for guess validation.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,25 @@
 use rand::Rng;
 use std::cmp::Ordering;
 
+struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value needs to be between 1 and 100, got {}.", value);
+        }
+        Guess {
+            value: value,
+        }
+    }
+
+    fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("Guess the number!");
 
@@ -11,17 +30,17 @@ fn main() {
         println!("Please input your guess.");
 
         let mut guess = String::new();
-        let num_bytes = std::io::stdin().read_line(&mut guess).expect("Failed to read line");
-        dbg!(num_bytes);
+        let _num_bytes = std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+//        dbg!(num_bytes);
 
-        let guess:u32 = match guess.trim().parse() {
-            Ok(num) => num,
+        let guess:Guess = match guess.trim().parse() {
+            Ok(num) => Guess::new(num),
             Err(_) => continue,
         };
 
-        println!("You guessed: {guess}");
+        println!("You guessed: {}", guess.value());
 
-        match guess.cmp(&secret_number) {
+        match guess.value().cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
             Ordering::Equal => {
