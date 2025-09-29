e6afb7cc8c7077a9a4d8985fea2dc6886b3191d8
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-09-29 22:33:54 +0000

    Return Result instead of panicking.
    Rename new to build.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
@@ -7,24 +8,27 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &str> {
         if args.len() < 3 {
-            panic!("not enough arguments");
+            return Err("not enough arguments");
         }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config {
+        Ok(Config {
             query,
             file_path,
-        }
+        })
     }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
