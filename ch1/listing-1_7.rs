d81f51b88d275788c77a96478c7f5c263dff419f
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-01-01 18:28:49 +0000

    Code comments from Listing 1.7.

--- ch1/listing-1_7.rs
+++ ch1/listing-1_7.rs
@@ -2,10 +2,11 @@ use std::rc::Rc;
 use std::sync::{Arc, Mutex};
 
 fn main() {
-    let a = 10;
-    let b = Box::new(20);
-    let c = Rc::new(Box::new(30));
-    let d = Arc::new(Mutex::new(40));
+    let a = 10; // Integer on the stack.
+    let b = Box::new(20); // Integer on the heap, also known as a boxed integer.
+    let c = Rc::new(Box::new(30)); // Boxed integer wrapped within a reference counter.
+    // Integer wrapped in an atomic reference counter and protected by a mutual exclusion lock.
+    let d = Arc::new(Mutex::new(40)); 
 
     println!("a: {a}, b: {b}, c: {c}, d: {:?}", d);
 }
