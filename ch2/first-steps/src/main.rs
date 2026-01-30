9d857c3cdea6260747c2acbce66c9bc03b309ff1
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-01-30 01:48:25 +0000

    Code comments from listing 2.2.

--- ch2/first-steps/src/main.rs
+++ ch2/first-steps/src/main.rs
@@ -1,12 +1,14 @@
 fn main() {
-    let a = 10;
-    let b: i32 = 20;
-    let c = 30i32;
+    let a = 10; // Types can be inferred by the compiler...
+    let b: i32 = 20; // ...or declared by the programmer when creating variables.
+    let c = 30i32; // Numeric types can include a type annotation in their literal form.
+    // Numbers can include underscores, which are intended to increase readability and have no functional impact.
     let d = 30_i32;
     let e = add(add(a, b), add(c, d));
     println!("(a + b) + (c + d) = {}", e);
 }
 
+// Type declarations are required when defining functions.
 fn add(i: i32, j: i32) -> i32 {
-    i + j
+    i + j // Functions return the last expression’s result so that return is not required.
 }
