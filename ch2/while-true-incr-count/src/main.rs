eeee7aaa1eea5bfa16059a0b48d98f7a9f000c7a
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-02-19 02:19:15 +0000

    Comment on Instant - Instant = Duration.

--- ch2/while-true-incr-count/src/main.rs
+++ ch2/while-true-incr-count/src/main.rs
@@ -1,10 +1,13 @@
 use std::time::{Duration, Instant};
 fn main() {
     let mut count = 0;
-    let time_limit = Duration::new(1, 0);
-    let start = Instant::now();
+    // https://doc.rust-lang.org/std/time/struct.Duration.html
+    let time_limit = Duration::new(1, 0); // Creates a Duration that represents 1 second.
+    // https://doc.rust-lang.org/std/time/struct.Instant.html
+    let start = Instant::now(); // Accesses time from the system’s clock.
 
     // An Instant minus an Instant returns a Duration.
+    // https://doc.rust-lang.org/std/time/struct.Instant.html#impl-Sub-for-Instant
     while (Instant::now() - start) < time_limit {
         count += 1;
     }
