9bffacfd232e964e5aa9ee1b1a48094ea43ba334
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-03-04 01:51:53 +0000

    Item is a pointer to a memory address.

--- ch2/needle-in-haystack/src/main.rs
+++ ch2/needle-in-haystack/src/main.rs
@@ -2,9 +2,10 @@ fn main() {
     let needle = 0o204; // Funny octal. See author source code.
     let haystack = [1, 1, 2, 5, 15, 52, 132, 203, 877, 4140, 21147]; // Different haystack, too.
 
+    // Highlight that item is a reference to a memory address that can be dereferenced.
     for item in &haystack {
         if *item == needle {
-            println!("{}", item);
+            println!("{} at address {:p}", item, item);
         }
     }
 }
