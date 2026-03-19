b66f3cb29aeb2959e1611d9e0abc3eab3c767691
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-03-18 21:51:12 -0500

    Link to API docs for TryInto trait.

--- ch2/type-conversion/try_into.rs
+++ ch2/type-conversion/try_into.rs
@@ -1,11 +1,13 @@
 // Not strictly necessary now, see Rust 2021 prelude. Book published in 2021.
 // https://doc.rust-lang.org/std/prelude/index.html
+// https://doc.rust-lang.org/std/convert/trait.TryInto.html
 use std::convert::TryInto;
 
 fn main() {
     let a: i32 = 10;
     let b: u16 = 100;
 
+    // try_into() returns a Result type that provides access to the conversion attempt.
     let b_ = b.try_into().unwrap();
 
     if a < b_ {
