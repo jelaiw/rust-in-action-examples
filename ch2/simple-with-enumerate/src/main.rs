61dbb0a18596625d87adf326da24cefc0614c557
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-03-21 23:27:21 -0500

    Link to API docs for Iterator.enumerate.

--- ch2/simple-with-enumerate/src/main.rs
+++ ch2/simple-with-enumerate/src/main.rs
@@ -6,6 +6,8 @@ dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
 
+    // Because lines() returns an iterator, it can be chained with enumerate().
+    // https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.enumerate
     for (i, line) in quote.lines().enumerate() {
         if line.contains(search_term) {
             let line_num = i + 1;
