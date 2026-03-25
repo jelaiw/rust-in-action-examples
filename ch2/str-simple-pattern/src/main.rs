6e367d6f5d662d62a19a33998023d49c77fd8d0b
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-03-25 16:02:26 -0500

    Comment on multiline strings and lines() iterator.
    Author code comments for Listing 2.18.

--- ch2/str-simple-pattern/src/main.rs
+++ ch2/str-simple-pattern/src/main.rs
@@ -1,11 +1,14 @@
 fn main() {
     let search_term = "picture";
+    // Multilined strings do not require special syntax. The \ character on line 4 escapes the new line.
     let quote = "\
 Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books.
 What do we seek through millions of pages?";
 
+    // lines() returns an iterator over quote where each iteration is a line of text. Rust uses each
+    // operating system’s conventions on what constitutes a new line.
     for line in quote.lines() {
         if line.contains(search_term) {
             println!("{}", line);
