b527ea5b5e9270ccc6793b350ede40c08aad737f
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-04-15 03:41:02 +0000

    Refactor duplicate code in if/else.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -23,33 +23,23 @@ fn handle_connection(mut stream: TcpStream) {
         .unwrap()
         .unwrap();    
 
-    if request_line == "GET / HTTP/1.1" {
-        let status_line = "HTTP/1.1 200 OK";
-        let contents = fs::read_to_string("hello.html").unwrap();
-        let length = contents.len();
+    let (status_line, file_name) = 
+        if request_line == "GET / HTTP/1.1" {
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        else {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        };
 
-        let response = format!(
-            "{status_line}\r\n\
-            Content-Length: {length}\r\n\
-            Content-Type: text/html\r\n\r\n\
-            {contents}
-        ");
+    let contents = fs::read_to_string(file_name).unwrap();
+    let length = contents.len();
 
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-    else {
-        let status_line = "HTTP/1.1 404 NOT FOUND";
-        let contents = fs::read_to_string("404.html").unwrap();
-        let length = contents.len();
+    let response = format!(
+        "{status_line}\r\n\
+        Content-Length: {length}\r\n\
+        Content-Type: text/html\r\n\r\n\
+        {contents}
+    ");
 
-        let response = format!(
-            "{status_line}\r\n\
-            Content-Length: {length}\r\n\
-            Content-Type: text/html\r\n\r\n\
-            {contents}
-        ");
-
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-
+    stream.write_all(response.as_bytes()).unwrap();
 }
