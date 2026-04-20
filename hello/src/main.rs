212c826ca72ae77091076cf4fc9170aaa2630ae2
jelaiw <14236583+jelaiw@users.noreply.github.com>
2026-04-20 22:43:49 +0000

    Rough out ThreadPool.
    Authors want us to try "compiler-driven development" :-D

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -6,14 +6,17 @@ use std::{
     time::Duration,
 };
 
+use hello::ThreadPool;
+
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     // https://doc.rust-lang.org/beta/std/net/struct.TcpListener.html#method.incoming
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
